c63616b157181cfdb551ff1a0779130431119b79
bitstone0610 <bitstone0610@outlook.com>
2019-07-24 15:48:03 +0200

    Added my commit sh file

---
+++ mycommits.sh
@@ -0,0 +1,27 @@
+for Y in {2013..2014}
+do
+  mkdir $Y
+  cd $Y
+  for M in {01..12}
+  do
+    mkdir $M
+    cd $M
+    for D in {01..28}
+    do
+      if [  "0" == "$(($RANDOM%3))"  ]; then  continue; fi
+      mkdir $D
+      cd $D
+      for i in $(seq 1 $(($RANDOM%7)))
+       do
+	echo "0$i on $M/$D/$Y" > commit.md
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:0$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:0$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "0$i on $M $D $Y"
+       done
+      cd ../
+    done
+    cd ../
+  done
+  cd ../
+done
